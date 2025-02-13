af0687237270ab900a12c810f3ab11bdcb986d27
Dejan Ristevski <dejanristevski96@gmail.com>
2025-02-13 15:53:33 +0100

    outputs added; backend init;

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-state-bucket-b10a8d4f0c8e62d60697e142e9e9cb1b"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = var.defaultRegion
 }
 
+// the security group
 resource "aws_security_group" "mock-sg" {
   name        = "mock-sg"
   description = "Security group for the mock instance"
@@ -21,6 +31,7 @@ resource "aws_security_group" "mock-sg" {
   }
 }
 
+// the ec2 instance
 resource "aws_instance" "mock-instance" {
   ami           = var.ami
   instance_type = "t2.micro"
